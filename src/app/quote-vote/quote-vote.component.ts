81088b55f71e2c604d567c984afbc95af0e2657e
mervinkogei <vincent.kogei@yahoo.com>
2019-10-26 22:52:07 +0300

    add the hightlight directive

--- src/app/quote-vote/quote-vote.component.ts
+++ src/app/quote-vote/quote-vote.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote-vote',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote-vote.component.css']
 })
 export class QuoteVoteComponent implements OnInit {
+  @Input() quote:Quote;
+  @Output() isComplete = new EventEmitter<boolean>();
+
+  quoteDelete(complete:boolean){
+    this.isComplete.emit(complete);
+  }
 
   constructor() { }
 
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,14 +8,60 @@ import { Quote } from '../quote';
 })
 export class QuoteComponent implements OnInit {
 
-  quote: Quote[] = [
-    new Quote(1, 'Motivation', 'Find an online version and watch merlin find his son'),
-    new Quote(2,'Attitude','I have to buy cookies for the parrot'),
-    new Quote(3,'Passion','Diana has her birthday coming up soon'),
-    new Quote(4,'Happiness','Pupper likes expensive snacks'),
-    new Quote(5,'Change','Damn Math'),
-    new Quote(6,'Inspiration','Cause I am an evil overlord'),
-  ];
+  public quotes: Quote[] = [
+    new Quote(1, "Kogei", "Humanity is acquiring all the right technology for all the wrong reasons.", "R. Buckminster Fuller", new Date(2019, 6, 7), 0, 0),
+    new Quote(2, "Kogei", "Technology is a word that describes something that doesn’t work yet.", "Douglas Adams", new Date(2019, 1, 12), 0, 0),
+    new Quote(3, "Kogei", "One machine can do the work of fifty ordinary men. No machine can do the work of one extraordinary man.", "Elbert Hubbard", new Date(201, 11, 7), 0, 0),
+    new Quote(4, "Kogei", "The art challenges the technology, and the technology inspires the art.", "John Lasseter", new Date(2019, 10, 7), 0, 0),
+  ]
+
+  addNewQuote(quote) {
+    let quoteLength = this.quotes.length;
+    quote.id = quoteLength + 1;
+    this.quotes.push(quote)
+  }
+
+  toggleDetails(index) {
+    this.quotes[index].showDescription = !this.quotes[index].showDescription;
+  }
+
+  quoteDelete(isComplete, index) {
+    if (isComplete) {
+      let toDelete = confirm(`Are you sure you want to delete ${this.quotes[index].name}?`)
+
+      if (toDelete) {
+        this.quotes.splice(index, 1)
+      }
+    }
+  }
+
+  numberOfLikes : number=0;
+  numberOfDislikes =0;
+
+  likeButtonClick(i) {
+    this.quotes[i].numberOfLikes++;
+  }
+
+  
+
+  dislikeButtonClick(i) {
+    this.quotes[i].numberOfDislikes++;
+  }
+
+  preNum:number
+  lastNum:number
+  counter:number
+
+  highestUpvote(){
+    this.preNum = 0
+    this.lastNum = 0
+
+    for(this.counter=0 ; this.counter < this.quotes.length; this.counter++) {
+      this.lastNum = this.quotes[this.counter].numberOfLikes;
+      if(this.lastNum > this.preNum){this.preNum = this.lastNum}
+    }
+    return  this.preNum
+  }
 
 
   constructor() { }
