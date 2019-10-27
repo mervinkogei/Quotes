571527c69242635f2fb631eac19ec35176b284c9
mervinkogei <vincent.kogei@yahoo.com>
2019-10-27 19:29:02 +0300

    add the filter date-count pipe to quote-detail component and modify the app.modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
-import { QuoteVoteComponent } from './quote-vote/quote-vote.component';
+// import { QuoteVoteComponent } from './quote-vote/quote-vote.component';
 import { DateCountPipe } from './date-count.pipe';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
 import { HighlightDirective } from './highlight.directive';
@@ -18,7 +18,7 @@ import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
   declarations: [
     AppComponent,
     QuoteComponent,
-    QuoteVoteComponent,
+    // QuoteVoteComponent,
     DateCountPipe,
     QuoteFormComponent,
     HighlightDirective,
--- src/app/quote-vote/quote-vote.component.ts
+++ src/app/quote-vote/quote-vote.component.ts
@@ -1,22 +1,22 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { Quote } from '../quote';
+// import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+// import { Quote } from '../quote';
 
-@Component({
-  selector: 'app-quote-vote',
-  templateUrl: './quote-vote.component.html',
-  styleUrls: ['./quote-vote.component.css']
-})
-export class QuoteVoteComponent implements OnInit {
-  @Input() quote:Quote;
-  @Output() isComplete = new EventEmitter<boolean>();
+// @Component({
+//   selector: 'app-quote-vote',
+//   templateUrl: './quote-vote.component.html',
+//   styleUrls: ['./quote-vote.component.css']
+// })
+// export class QuoteVoteComponent implements OnInit {
+//   @Input() quote:Quote;
+//   @Output() isComplete = new EventEmitter<boolean>();
 
-  quoteDelete(complete:boolean){
-    this.isComplete.emit(complete);
-  }
+//   quoteDelete(complete:boolean){
+//     this.isComplete.emit(complete);
+//   }
 
-  constructor() { }
+//   constructor() { }
 
-  ngOnInit() {
-  }
+//   ngOnInit() {
+//   }
 
-}
+// }
