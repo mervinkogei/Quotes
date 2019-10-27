08a6d45119fa812216e1a95b16593ffc6c82a2f4
mervinkogei <vincent.kogei@yahoo.com>
2019-10-27 23:13:13 +0300

    add the form module to the app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
-// import { QuoteVoteComponent } from './quote-vote/quote-vote.component';
-import { DateCountPipe } from './date-count.pipe';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
-import { HighlightDirective } from './highlight.directive';
 import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
+import { DateCountPipe } from './date-count.pipe';
+import { HighlightDirective } from './highlight.directive';
 
 
 
@@ -18,16 +16,17 @@ import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
   declarations: [
     AppComponent,
     QuoteComponent,
-    // QuoteVoteComponent,
-    DateCountPipe,
     QuoteFormComponent,
+    QuoteDetailComponent,
+    DateCountPipe,
     HighlightDirective,
-    QuoteDetailComponent
+    
+  
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-   
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
