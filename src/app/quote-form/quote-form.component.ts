16aa0e5dff5bdf82431550e7eb04db6e8aa6343e
mervinkogei <vincent.kogei@yahoo.com>
2019-10-27 18:08:51 +0300

    add validation to quote-form input type

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import { Quote } from '../quote';
+import { Quote} from '../quote';
+
 
 @Component({
   selector: 'app-quote-form',
@@ -7,11 +8,13 @@ import { Quote } from '../quote';
   styleUrls: ['./quote-form.component.css']
 })
 export class QuoteFormComponent implements OnInit {
-  newQuote = new Quote('', '', '', new Date(), 0, 0);
-  @Output() addQoute = new EventEmitter<Quote>();
 
-  ongezaQuote() {
-    this.addQoute.emit(this.newQuote);
+  newQuote=new Quote(0,"","","",new Date(),0,0);
+
+  @Output() addQuote = new EventEmitter<Quote>();
+
+  submitQuote(){
+    this.addQuote.emit(this.newQuote);
     this.newQuote = new Quote(0,"","","",new Date(),0,0)
   }
 
--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,7 +1,8 @@
 export class Quote {
 
 showDescription: boolean;
-constructor(public id: number,
+constructor(
+public id: number,
 public name: string,
 public description: string,
 public theQuote: string,
